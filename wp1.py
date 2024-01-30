e3b51ef3d39449274d44b9f098b9e05b935fa499
kcpalaci <156475096+kcpalaci@users.noreply.github.com>
2024-01-29 17:05:21 -0800

    Adjusted the bounds of the bytes being read so that the file reads from the first byte to the last rather than the second byte to the first byte.

--- wp1.py
+++ wp1.py
@@ -7,19 +7,19 @@ import sys
 
 
 def read_file():
-    with open(sys.argv[0], "r") as original_file:
+    with open(sys.argv[1], "r") as original_file:
         original_file.seek(0,2)
         num_bytes = original_file.tell()
         byte_list = []
         original_file.seek(0)
         for i in range(num_bytes):
             byte_list.append(original_file.read(1))
-            original_file.seek(i)
+            original_file.seek(i+1)
         return reversed(byte_list)
     
 
 def encrypt_file(rev_byte_list):
-    with open(sys.argv[1], "w") as encrypted_file:
+    with open(sys.argv[2], "w") as encrypted_file:
         for byte in rev_byte_list:
             encrypted_file.write(byte)
 
