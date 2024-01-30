b938d25f587a141913634202381d3867670f81d7
kcpalaci <156475096+kcpalaci@users.noreply.github.com>
2024-01-29 17:10:30 -0800

    Updated code to adjust for FileNotFoundErrors and empty text file inputs.

--- wp1.py
+++ wp1.py
@@ -7,15 +7,21 @@ import sys
 
 
 def read_file():
-    with open(sys.argv[1], "r") as original_file:
-        original_file.seek(0,2)
-        num_bytes = original_file.tell()
-        byte_list = []
-        original_file.seek(0)
-        for i in range(num_bytes):
-            byte_list.append(original_file.read(1))
-            original_file.seek(i+1)
-        return reversed(byte_list)
+    try:
+        with open(sys.argv[1], "r") as original_file:
+            original_file.seek(0,2)
+            num_bytes = original_file.tell()
+            byte_list = []
+            original_file.seek(0)
+            for i in range(num_bytes):
+                byte_list.append(original_file.read(1))
+                original_file.seek(i+1)
+            if not byte_list:
+                print("\nYou provided an empty txt file. Please enter a non-empty file to encrypt.\n")
+                sys.exit()
+            return reversed(byte_list)
+    except FileNotFoundError:
+        print("\nYour file could not be found. Please enter a valid, existing txt file.\n")
     
 
 def encrypt_file(rev_byte_list):
