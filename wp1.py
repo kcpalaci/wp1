38254e273fc5158d6970c8741293e9b6dcd18d92
kcpalaci <156475096+kcpalaci@users.noreply.github.com>
2024-01-29 16:55:02 -0800

    Created read_file function and encrypt_file function. Used sys.argv to open given files

--- wp1.py
+++ wp1.py
@@ -3,5 +3,27 @@
 #Date Created: 01/29/2024
 
 
+import sys
+
+
+def read_file():
+    with open(sys.argv[0], "r") as original_file:
+        original_file.seek(0,2)
+        num_bytes = original_file.tell()
+        byte_list = []
+        original_file.seek(0)
+        for i in range(num_bytes):
+            byte_list.append(original_file.read(1))
+            original_file.seek(i)
+        return reversed(byte_list)
+    
+
+def encrypt_file(rev_byte_list):
+    with open(sys.argv[1], "w") as encrypted_file:
+        for byte in rev_byte_list:
+            encrypted_file.write(byte)
+
+
 if __name__ == "__main__":
-    pass
+    rev_byte_list = read_file()
+    encrypt_file(rev_byte_list)
